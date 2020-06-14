7ddf61fb82d7f8f21c2d678bc06897bcffdc3b29
Giusy Castiglione <gc@datespotz.com>
2020-06-14 00:12:02 -0400

    edited bio

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,13 +5,14 @@ const About = (props) => {
     <div className="wrapper">
     	<div className="content">
     		<div className="highlight_description">
-    		<p>Idioteque is a 21 year old producer from Bournemouth, UK</p>
+    		<p>Idioteque is a 21 year old music producer from Bournemouth, UK</p>
 
-<p>Developing a passion and eclectic taste for music from a very young age, he began playing musical instruments at age 9 and producing his own music at age 14, taking inspiration from various genres such as Rock, Metal, Alternative, Hip Hop, and Dance Music.
+<p>Developing a passion and eclectic taste for music from a very young age, he began playing musical instruments at age 11 and producing his own music at age 14, taking inspiration from various genres such as Rock, Metal, Alternative, Hip Hop, and Dance Music.
 
 At age 17, Idioteque started djing and spending more time around computer-based music production, focusing primarily on Drum and Bass.
-Since starting his musical journey, he has become a well-known figure on the local Drum and Bass circuit, supporting some of the biggest names in the scene at clubs in his home town. As of 2018, he has released heavyweight content on the likes of BHA, Culture, Oscity, and Sine Audio.
-In Summer 2019, Idioteque relocated to London to further expand his network, meet like-minded individuals, and better himself as a musician. While in London, he has slowly but surely began to make a name for himself. In February 2020 he founded an events venture called “Immersed”, focused on showcasing underground electronic musicians/artists accompanied by an intimate setting, for those who share the same mutual love and appreciation for the music. 
+As of 2018, he has released heavyweight content on the likes of BHA, Culture, Oscity, and Sine Audio and has gone to support some of the biggest names in the scene at clubs in his home town. 
+Since starting his musical journey, he has become a well-known figure on the local Drum and Bass circuit.
+In Summer 2019, Idioteque relocated to London to further expand his network, meet like-minded individuals, and better himself as a musician. While in London, he has slowly but surely began to make a name for himself. In February 2020 he founded an events venture called “Immerse”, focused on showcasing underground electronic musicians/artists accompanied by an intimate setting, for those who share the same mutual love and appreciation for the music. 
 
 The future looks promising for Idioteque, which is seeing him delve into the 140 BPM realm with possibly his strongest work yet, a welcome change from his previous Drum and Bass releases.</p>
 
